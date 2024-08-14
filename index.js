605558debbd7eec8ff90de7b55c2fab741fc1537
ankitakangane <kanganeankita1@gmail.com>
2024-08-14 08:09:01 +0530

    different function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // name function
+
 function sayHii(name){
     console.log(`hii ${name}`)
 }
@@ -6,4 +7,41 @@ sayHii("ankita");
 sayHii("nikhil");
 sayHii("yash");
 
+
+// higher order and call back function 
+
+function sendEmail(name){
+    console.log(`Email send to ${name}`);
+    return 3;
+
+}
+
+
+function sayGreeting(name,abc){
+console.log(`Hello ${name}`);
+abc(name);
+}
+sayGreeting("Pooja",sendEmail)
+
+
+// arrow function
+
+function date(){
+    console.log(`${new Date()}`)
+}
+setInterval(date,2000)
+
+
+//arrow fun
+setInterval(
+    () => {
+    console.log(`${new Date()}`)
+}
+,2000);
+
+
+ //IIFE
+ (() => {
+    console.log(`${new Date()}`)
+})()
 
